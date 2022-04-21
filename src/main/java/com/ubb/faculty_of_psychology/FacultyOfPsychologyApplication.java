7304a621309f98ec6b66df2bf757db646df84c14
raduceaca1234 <ceaca.radu@yahoo.com>
2022-04-21 17:20:39 +0300

    Project structure

---
+++ src/main/java/com/ubb/faculty_of_psychology/FacultyOfPsychologyApplication.java
@@ -0,0 +1,13 @@
+package com.ubb.faculty_of_psychology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyOfPsychologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyOfPsychologyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubb/faculty_of_psychology/FacultyOfPsychologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubb.faculty_of_psychology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyOfPsychologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
